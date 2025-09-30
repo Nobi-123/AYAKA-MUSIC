4b5469b434bb256b888f831a4218e9b54312fce3
Nobi-123 <nobita.infringement.report@gmail.com>
2025-09-30 16:31:03 +0530

    Update __init__.py

--- SONALI/__init__.py
+++ SONALI/__init__.py
@@ -9,8 +9,12 @@ from .core.userbot import Userbot
 # Misc and utils
 from .misc import dbb, heroku
 
-# Modules
-from .modules import chat_and_respond, text_to_voice, react_to_message, STICKERS, is_chat_enabled, enable_chat, disable_chat, last_bot_message, OWNER_USERNAME
+# SONALI/modules/__init__.py
+from .stickers import STICKERS, get_context_sticker
+from .chatbot import chat_and_respond, last_bot_message, OWNER_USERNAME
+from .voice_manager import text_to_voice
+from .reactions import react_to_message
+from .chat_control import is_chat_enabled, enable_chat, disable_chat
 
 # APIs (placeholder, update if you have these classes)
 from .platforms import AppleAPI, CarbonAPI, SoundAPI, SpotifyAPI, RessoAPI, TeleAPI, YouTubeAPI
