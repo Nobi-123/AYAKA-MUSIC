5bc8fbce56a6b62248c42e195eaa3102f75a22b8
Nobi-123 <nobita.infringement.report@gmail.com>
2025-09-30 16:26:56 +0530

    Update __init__.py

--- SONALI/modules/__init__.py
+++ SONALI/modules/__init__.py
@@ -1,16 +1,7 @@
 # SONALI/modules/__init__.py
 
-# Stickers
 from .stickers import STICKERS, get_context_sticker
-
-# Chatbot
 from .chatbot import chat_and_respond, last_bot_message, OWNER_USERNAME
-
-# Voice
 from .voice_manager import text_to_voice
-
-# Reactions
 from .reactions import react_to_message
-
-# Chat control
 from .chat_control import is_chat_enabled, enable_chat, disable_chat
